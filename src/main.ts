a492b3a35361abb4265215bfffbed4e2e05960a4
Mostafa Elgaml <elgaml.work@gmail.com>
2024-07-31 20:35:22 +0300

    update port 2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4560);
+  await app.listen(process.env.PORT, '0.0.0.0');
   console.log(await app.getUrl());
 }
 bootstrap();
